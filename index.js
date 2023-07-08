87e504894dd6981623f26785375568e978d8af5b
tetriv0217 <131021398+tetriv0217@users.noreply.github.com>
2023-07-08 12:19:48 +0530

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var playerName1 = prompt("Enter the name of first player:- ");
+var playerName2 = prompt("Enter the name of Second player:- ");
 var player1 = Math.random();
 player1 = player1 * 6 + 1;
 player1 = Math.floor(player1);
@@ -7,6 +9,8 @@ player2 = player2 * 6 + 1;
 player2 = Math.floor(player2);
 var randomDiceImage2 = "./images/dice" + player2 + ".png";
 console.log(player1 + " " + player2);
+document.querySelectorAll("p")[0].innerHTML=playerName1;
+document.querySelectorAll("p")[1].innerHTML=playerName2;
 document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1);
 document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
 
@@ -14,8 +18,8 @@ if (player1 === player2) {
     document.querySelector(".title").textContent = "Draw!";
 }
 else if (player1 > player2) {
-    document.querySelector(".title").textContent = "🚩Player 1 Wins!";
+    document.querySelector(".title").textContent = "🚩"+playerName1+" Wins!";
 }
 else {
-    document.querySelector(".title").textContent = "Player 2 Wins!🚩";
+    document.querySelector(".title").textContent = "🚩"+playerName2+" Wins!";
 }
